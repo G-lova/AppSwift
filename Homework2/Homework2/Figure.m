e25f49346efd2712af3338f0020aa2d8e326e606
G-lova <suslik.lyuba@gmail.com>
2024-01-18 15:40:20 +0300

    Homework2 added

---
+++ Homework2/Homework2/Figure.m
@@ -0,0 +1,24 @@
+//
+//  Figure.m
+//  Homework2
+//
+//  Created by User on 18.01.2024.
+//
+
+#import "Figure.h"
+
+@implementation Figure
+
+- (float) calculateArea {
+    return 0;
+}
+
+- (float) calculatePerimetr {
+    return  0;
+}
+
+- (void) printInfo {
+    
+}
+
+@end
---
+++ Homework2/Homework2/main.m
@@ -0,0 +1,25 @@
+//
+//  main.m
+//  Homework2
+//
+//  Created by User on 18.01.2024.
+//
+
+#import <Foundation/Foundation.h>
+#import "Rectangle.h"
+#import "Circle.h"
+#import "Triangle.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        Rectangle *rectangle = [[Rectangle alloc] initWithWidth:5 andHeight:3];
+        Circle *circle = [[Circle alloc] initWithRadius:2.5];
+        Triangle *triangle = [[Triangle alloc] initWithSide1:3 andSide2:4 andSide3:5];
+        
+        NSArray *figures = @[rectangle, circle, triangle];
+        for (Figure *figure in figures) {
+            [figure printInfo];
+        }
+    }
+    return 0;
+}
