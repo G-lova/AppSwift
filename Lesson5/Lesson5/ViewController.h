75668b35973cb9cf84ebc8a6bbad6127dd389eb5
G-lova <suslik.lyuba@gmail.com>
2024-01-26 21:16:05 +0300

    Lesson5 added

---
+++ Lesson5/Lesson5/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Lesson5
+//
+//  Created by User on 26.01.2024.
+//
+
+#import <UIKit/UIKit.h>
+#import "Loader.h"
+
+@interface ViewController : UIViewController
+
+@property (nonatomic, strong) Loader *loader;
+
+@end
+
